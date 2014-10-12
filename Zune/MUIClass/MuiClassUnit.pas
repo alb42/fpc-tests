db843fd185ec7cfeeeba1a15972855d96fb4d53e
alb <alb42@web.de>
2014-10-12 16:25:15 +0100

    Zune: Example for an own Zune Class with custom drawing

---
+++ Zune/MUIClass/MuiClassUnit.pas
@@ -0,0 +1,287 @@
+unit MuiClassUnit;
+
+interface
+
+{$mode Delphi}{$H+}
+
+uses
+  Classes, Exec, Intuition, Mui, AGraphics, TagsArray, sysutils;
+
+type
+  TMUICanvas = record
+    RastPort: PRastPort;
+    DrawRect: TRect;
+  end;
+
+  TMUIBase = class
+  private
+    FParent: TMUIBase;
+    function GetIsHandle: Boolean;
+    procedure SetParent(AParent: TMUIBase);
+  protected
+    FMUIObject: PObject_;
+    FParentTarget: PObject_;
+  public
+    function CreateHandle: Boolean; virtual; abstract;
+    procedure DestroyHandle; virtual; abstract;
+
+    procedure AddChild(const NChild: TMUIBase);
+    procedure RemoveChild(const NChild: TMUIBase);
+
+    property MUIObject: PObject_ read FMUIObject;
+    property IsHandle: Boolean read GetIsHandle;
+    property Parent: TMUIBase read FParent write SetParent;
+  end;
+
+  TMUIWinClass = class(TMUIBase)
+    function CreateHandle: Boolean; override;
+  end;
+
+  TMUIAppClass = class(TMUIBase)
+    function CreateHandle: Boolean; override;
+  end;
+
+
+  TMUIBaseClass = class(TMUIBase)
+  private
+    FLeft: Integer;
+    FTop: Integer;
+    FWidth: Integer;
+    FHeight: Integer;
+    FOnDraw: TNotifyEvent;
+    FMuiCanvas: TMUICanvas;
+  public
+    Magic: Integer;
+    constructor Create;
+    function CreateHandle: Boolean; override;
+    procedure DestroyHandle; override;
+
+    property Left: Integer read FLeft write FLeft;
+    property Top: Integer read FTop write FTop;
+    property Width: Integer read FWidth write FWidth;
+    property Height: Integer read FHeight write FHeight;
+    property MUICanvas: TMUICanvas read FMUICanvas;
+
+    property OnDraw: TNotifyEvent read FOnDraw write FOnDraw;
+
+  end;
+
+var
+  ALBsClass: PIClass;
+
+implementation
+
+var
+  SuperClass: PIClass;
+
+function TMUIBase.GetIsHandle: Boolean;
+begin
+  Result := Assigned(FMUIObject);
+end;
+
+procedure TMUIBase.SetParent(AParent: TMUIBase);
+begin
+  if AParent = FParent then
+    Exit;
+  if Assigned(FParent) then
+    FParent.RemoveChild(Self);
+  if Assigned(AParent) then
+    AParent.AddChild(Self);
+  FParent := AParent;
+end;
+
+procedure TMUIBase.AddChild(const NChild: TMUIBase);
+begin
+  DoMethod(FParentTarget, OM_ADDMEMBER, [NChild.MUIObject]);
+end;
+
+procedure TMUIBase.RemoveChild(const NChild: TMUIBase);
+begin
+  DoMethod(FParentTarget, OM_REMMEMBER, [NChild.MUIObject]);
+end;
+
+
+constructor TMUIBaseClass.Create;
+begin
+  FOnDraw := nil;
+end;
+
+function TMUIWinClass.CreateHandle: Boolean;
+var
+  Tags: TTagsList;
+begin
+  Result := False;
+  AddTags(Tags, [Tag_Done, 0, TAG_Done, 0]);
+  FParentTarget := MUI_NewObject(MUIC_Group,
+    [TAG_END, TAG_END]);
+  FMUIObject := MUI_NewObject(MUIC_Window,
+    [(MUIA_Window_RootObject), FParentTarget,
+     TAG_END]);
+
+  if Assigned(FMUIObject) then
+  begin
+    Pointer(INST_DATA(ALBsClass, Pointer(FMUIObject))^) := Self;
+    Result := True;
+  end;
+end;
+
+
+
+function TMUIAppClass.CreateHandle: Boolean;
+var
+  Tags: TTagsList;
+begin
+  Result := False;
+  AddTags(Tags, [Tag_Done, 0, TAG_Done, 0]);
+  FMUIObject := MUI_NewObject(MUIC_Application, [0,0]);
+  FParentTarget := FMUIObject;
+  if Assigned(FMUIObject) then
+  begin
+    Pointer(INST_DATA(ALBsClass, Pointer(FMUIObject))^) := Self;
+    Result := True;
+  end;
+end;
+
+
+function TMUIBaseClass.CreateHandle: Boolean;
+var
+  Tags: TTagsList;
+begin
+  Result := False;
+  AddTags(Tags, [Tag_Done, 0, TAG_Done, 0]);
+  FMUIObject := NewObjectA(ALBsClass, nil, GetTagPtr(Tags));
+  FParentTarget := FMUIObject;
+  if Assigned(FMUIObject) then
+  begin
+    Pointer(INST_DATA(ALBsClass, Pointer(FMUIObject))^) := Self;
+    Result := True;
+  end;
+end;
+
+procedure TMUIBaseClass.DestroyHandle;
+begin
+  if IsHandle then
+  begin
+    Parent := nil;
+    MUI_DisposeObject(FMUIObject);
+    FMUIObject := nil;
+  end;
+end;
+
+
+procedure DestroyClass;
+begin
+  if Assigned(ALBsClass) then
+    FreeClass(ALBsClass);
+  if Assigned(SuperClass) then
+    MUI_FreeClass(SuperClass);
+end;
+
+function Dispatcher(cl: PIClass; Obj: PObject_; Msg: PMsg): LongWord; cdecl;
+var
+  AskMsg: PMUIP_AskMinMax;
+  ri: PMUI_RenderInfo;
+  rp: PRastPort;
+  Region: PRegion;
+  r: TRectangle;
+  clip: Pointer;
+  MUIB: TMUIBaseClass;
+begin
+  //write('Enter Dispatcher with: ');
+  case Msg^.MethodID of
+    OM_NEW:begin
+      writeln('NEW');
+      Result := DoSuperMethodA(cl, obj, msg);
+    end;
+    OM_DISPOSE: begin
+      writeln('DISPOSE');
+      Result := DoSuperMethodA(cl, obj, msg);
+    end;
+    MUIM_AskMinMax: begin
+      Result := DoSuperMethodA(cl, obj, msg);
+      AskMsg := Pointer(Msg);
+      AskMsg^.MinMaxInfo^.MinWidth := AskMsg^.MinMaxInfo^.MinWidth + 25;
+      AskMsg^.MinMaxInfo^.DefWidth := AskMsg^.MinMaxInfo^.DefWidth + 250;
+      AskMsg^.MinMaxInfo^.MaxWidth := AskMsg^.MinMaxInfo^.MaxWidth + 2500;
+      AskMsg^.MinMaxInfo^.MinHeight := AskMsg^.MinMaxInfo^.MinHeight + 18;
+      AskMsg^.MinMaxInfo^.DefHeight := AskMsg^.MinMaxInfo^.DefHeight + 180;
+      AskMsg^.MinMaxInfo^.MaxHeight := AskMsg^.MinMaxInfo^.MaxHeight + 1800;
+      writeln('set minwidth to ', AskMsg^.MinMaxInfo^.MinWidth);
+      Result := 0;
+      writeln('AskMinMax');
+    end;
+    MUIM_Draw: begin
+      writeln('DRAW');
+      Result := DoSuperMethodA(cl, obj, msg);
+      if PMUIP_Draw(msg)^.Flags and MADF_DRAWOBJECT = 0 then
+      begin
+        Exit;
+      end;
+      rp := nil;
+      ri := MUIRenderInfo(Obj);
+      if Assigned(ri) then
+        rp := ri^.mri_RastPort;
+      if Assigned(rp) then
+      begin
+        MUIB:= TMUIBaseClass(INST_DATA(cl, Pointer(obj))^);
+        clip := MUI_AddClipping(ri, Obj_Left(obj), Obj_top(Obj), Obj_Width(Obj), Obj_Height(Obj));
+        if Assigned(MUIB) then
+        begin
+          MUIB.FMUICanvas.RastPort := rp;
+          MUIB.FMUICanvas.DrawRect := Rect(Obj_mLeft(Obj), Obj_mTop(Obj), Obj_mRight(Obj), Obj_mBottom(Obj));
+          if Assigned(MUIB.FOnDraw) then
+          begin
+            MUIB.FOnDraw(MUIB);
+          end;
+        end;
+
+        {SetAPen(rp, 1);
+        RectFill(rp, Obj_mLeft(Obj), Obj_mTop(Obj), Obj_mRight(Obj), Obj_MBottom(Obj));
+        SetAPen(rp, 2);
+        RectFill(rp, Obj_mLeft(Obj) + 10, Obj_mTop(obj) + 10, Obj_mLeft(obj) + 20, Obj_mTop(obj) + 20);
+        SetAPen(rp, 3);
+        Move(rp, 20, 5);
+        Draw(rp, Obj_Right(Obj) + 25, 200);
+        Move(rp, Obj_Right(Obj) - 5, 50);
+        Draw(rp, Obj_Left(Obj) - 25, Obj_Bottom(Obj));}
+        MUI_RemoveClipRegion(ri, clip);
+      end;
+      Result := 0;
+    end;
+    else
+    begin
+      //writeln('unknown ', Msg^.MethodID);
+      Result := DoSuperMethodA(cl, obj, msg);
+    end;
+  end;
+end;
+
+
+procedure CreateClass;
+begin
+  SuperClass := MUI_GetClass(MUIC_Group);
+  if not Assigned(SuperClass) then
+  begin
+    writeln('Superclass for the new class not found.');
+    Exit;
+  end;
+  ALBsClass := MakeClass(nil ,nil, SuperClass, SizeOf(Pointer), 0);
+  if not Assigned(ALBsClass) then
+  begin
+    writeln('Cannot make class.');
+    DestroyClass;
+    Exit;
+  end;
+  ALBsClass^.cl_Dispatcher.h_Entry := IPTR(@Dispatcher);
+  ALBsClass^.cl_Dispatcher.h_SubEntry := 0;
+  ALBsClass^.cl_Dispatcher.h_Data := nil;
+
+end;
+
+
+
+initialization
+  CreateClass;
+finalization
+  DestroyClass;
+end.
---
+++ Zune/MUIClass/SimpleMUI.pas
@@ -0,0 +1,170 @@
+program SimpleMUI;
+
+{$mode objfpc}{$H+}
+
+uses
+  tagsarray, Classes, SysUtils, Exec, AmigaDos, Intuition,
+  Mui, Utility, AGraphics, MUIClassUnit;
+
+const
+  Esc33c = #27#99;
+
+type
+  TestClass = class
+    procedure DrawIt(Sender: TObject);
+    procedure DrawIt2(Sender: TObject);
+  end;
+
+procedure Buttonfunc(Hook: PHook; Obj: PObject_; Msg:Pointer); cdecl;
+begin
+  writeln('button clicked');
+end;
+
+procedure TestClass.DrawIt2(Sender: TObject);
+var
+  M: TMUIBaseClass;
+  s: String;
+begin
+  M := TMUIBaseClass(Sender);
+  with M.MUICanvas do
+  begin
+    SetAPen(RastPort, 1);
+    GfxMove(RastPort, DrawRect.Left + 10, DrawRect.Top + 10);
+    s := 'Test';
+    GfxText(RastPort, PChar(s), Length(s));
+  end;
+end;
+
+procedure TestClass.DrawIt(Sender: TObject);
+var
+  M: TMUIBaseClass;
+  rp: PRastPort;
+begin
+  writeln('enter drawit');
+  M := TMUIBaseClass(Sender);
+  with M.MUICanvas do
+  begin
+    SetAPen(RastPort, 1);
+    RectFill(RastPort, DrawRect.Left, DrawRect.Top, DrawRect.Right, DrawRect.Bottom);
+    SetAPen(RastPort, 2);
+    RectFill(RastPort, DrawRect.Left + 10, DrawRect.Top + 10, DrawRect.Left + 20, DrawRect.Top + 20);
+    SetAPen(RastPort, 3);
+    GfxMove(RastPort, 20, 5);
+    Draw(RastPort, DrawRect.Right + 25, 200);
+    GfxMove(RastPort, DrawRect.Right - 5, 50);
+    Draw(RastPort, DrawRect.Left - 25, DrawRect.Bottom);
+  end;
+  writeln('leave Drawit');
+end;
+
+function MyGet(o: pObject_; tag: LongWord): LongWord;
+var
+  Res: LongWord;
+begin
+  GetAttr(tag, o, @Res);
+  MyGet := Res;
+end;
+
+var
+  cust, Cust2, wnd, app, but, txt, grp: pObject_;
+  sigs: LongWord;
+  ButtonHook: THook;
+  Tags: TTagsList;
+  MUIB: TMUIBaseClass;
+  MUIB2: TMUIBaseClass;
+  TestC: TestClass;
+
+  Mapp: TMUIAppClass;
+  MWin: TMUIWinClass;
+begin
+  TestC := TestClass.create;
+  MUIB := TMUIBaseClass.create;
+  MUIB.OnDraw := @TestC.DrawIt;
+  MUIB.CreateHandle;
+  MUIB2 := TMUIBaseClass.create;
+  MUIB2.OnDraw := @TestC.DrawIt2;
+  MUIB2.CreateHandle;
+
+
+  ButtonHook.h_Entry := IPTR(@buttonFunc);
+
+  but := MUI_MakeObject(MUIO_Button, [PChar('_Ok')]);
+
+  AddTags(Tags, [Tag_Done, 0, TAG_Done, 0]);
+  if Assigned(ALBsClass) then
+  begin
+    //Cust := NewObjectA(ALBsClass, nil, GetTagPtr(Tags));
+    //Cust2 := NewObjectA(ALBsClass, nil, GetTagPtr(Tags));
+  end;
+
+
+  Mapp := TMUIAppClass.Create;
+  MApp.CreateHandle;
+
+  MWin := TMUIWinClass.Create;
+  MWin.CreateHandle;
+
+  wnd := MWin.MUIObject;
+  MWin.Parent := MApp;
+  MUIB.Parent := MWin;
+  MUIB2.Parent := MWin;
+  app := Mapp.MUIObject;
+  SetAttrs(app, [MUIA_Application_Window, wnd, TAG_END]);
+
+
+  {txt := MUI_NewObject(MUIC_Text,
+    [LongInt(MUIA_Text_Contents), PChar(Esc33c + 'Hello world'#10'How are you?'),
+     TAG_END]);
+
+  grp := MUI_NewObject(MUIC_Group,
+    [LongInt(MUIA_Group_Child), MUIB.MUIObject,
+     LongInt(MUIA_Group_Child), txt,
+     LongInt(MUIA_Group_Child), MUIB2.MUIObject,
+     LongInt(MUIA_Group_Child), but,
+     TAG_END]);
+
+  wnd := MUI_NewObject(MUIC_Window,
+    [LongInt(MUIA_Window_Title), PChar('Hello World'),
+     LongInt(MUIA_Window_RootObject), grp,
+     TAG_END]);
+
+  app := MUI_NewObject(MUIC_Application,
+    [LongInt(MUIA_Application_Window), wnd,
+     TAG_END]);}
+  writeln('Start');
+  if app <> nil then
+  begin
+    writeln('app ok');
+    CallHook(PHook(OCLASS(wnd)), wnd,
+      [MUIM_Notify, MUIA_Window_CloseRequest, True,
+      LongWord(app), 2,
+      MUIM_Application_ReturnID, MUIV_Application_ReturnID_Quit]);
+    (*
+    CallHook(PHook(OCLASS(but)), but,
+      [MUIM_Notify, MUIA_Pressed, False,
+      LongWord(app), 2,
+      MUIM_CallHook, @ButtonHook]);
+   *)
+    sigs := 0;
+    writeln('open it');
+    SetAttrs(wnd, [MUIA_Window_Open, True, TAG_END]);
+    if myGet(wnd, MUIA_Window_Open)<>0 then
+    begin
+      writeln('Window open');
+      while Integer(CallHook(PHook(OCLASS(app)), app,
+                     [MUIM_Application_NewInput, @sigs]))
+            <> MUIV_Application_ReturnID_Quit do
+      begin
+        if (sigs <> 0) then
+        begin
+          sigs := Wait(sigs or SIGBREAKF_CTRL_C);
+          if (Sigs and SIGBREAKF_CTRL_C) <>0 then
+            Break;
+        end;
+      end;
+    end else
+      writeln('Window not open');
+    MUI_DisposeObject(app);
+  end;
+end.
+
