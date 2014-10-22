c545f4e63cf16e88fbe4540264fd063c6c827b24
alb <alb42@web.de>
2014-10-22 19:28:53 +0100

    Labeltest: different Font styles

--- lcl/Label/unit1.pas
+++ lcl/Label/unit1.pas
@@ -15,10 +15,17 @@ type
     Button1: TButton;
     Button2: TButton;
     Label1: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
     Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
