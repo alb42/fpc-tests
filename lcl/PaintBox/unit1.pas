f873c50fd7a9775b183830f2add2d06b68826b09
alb <alb42@web.de>
2014-10-22 21:37:49 +0100

    Paintbox, FillRect example

--- lcl/PaintBox/unit1.pas
+++ lcl/PaintBox/unit1.pas
@@ -33,6 +33,7 @@ procedure TForm1.PaintBox1Paint(Sender: TObject);
 begin
   with PaintBox1.Canvas do
   begin
+    // Line/Pen.Color Tests
     Pen.Color := clLime;
     MoveTo(10,10);
     LineTo(PaintBox1.Width - 50, PaintBox1.Height - 50);
@@ -40,14 +41,20 @@ begin
     LineTo(5, PaintBox1.Height - 40);
     Pen.Color := clBlue;
     LineTo(50,6);
+    // Rectangle/Brush Test
     Pen.Color := clYellow;
     Brush.Color := clGreen;
     Rectangle(35,25,90,99);
+    // FillRect Test
+    Brush.Color := clFuchsia;
+    FillRect(100, 150, 175, PaintBox1.Height - 100);
+    // Text test
     Pen.Color := clRed;
     Font.Color := clRed;
     Brush.Color := clWhite;
     Brush.Style:= bsClear;
     TextOut(100, 100, 'Test Text Clear');
+    Font.Color := clYellow;
     Brush.Color := clBlue;
     Brush.Style:= bsSolid;
     TextOut(100, 120, 'Test Text Solid');
