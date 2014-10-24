7b5fccc83f760c3c1adf99bf60fcb6f0b09f5c3a
alb <alb42@web.de>
2014-10-24 19:48:03 +0100

    lcl: Image example

---
+++ lcl/Image/unit1.pas
@@ -0,0 +1,40 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    CheckBox1: TCheckBox;
+    Image1: TImage;
+    procedure CheckBox1Change(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.CheckBox1Change(Sender: TObject);
+begin
+  Image1.Stretch := CheckBox1.Checked;
+end;
+
+end.
+
