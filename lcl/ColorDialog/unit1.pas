8155306058149b3bd9b8bb25ac3aacb50360d2e8
alb <alb42@web.de>
2015-03-30 21:53:33 +0100

    LCL: ColorDialog example

---
+++ lcl/ColorDialog/unit1.pas
@@ -0,0 +1,45 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    ColorDialog1: TColorDialog;
+    Panel1: TPanel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if ColorDialog1.Execute then
+  begin
+    Panel1.Color := ColorDialog1.Color;
+    PAnel1.Invalidate;
+  end;
+end;
+
+end.
+
