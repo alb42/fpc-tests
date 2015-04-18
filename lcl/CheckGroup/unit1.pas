706df70089b85e9c3a0b5293b4b11916cf3a6045
alb <alb42@web.de>
2015-04-18 19:22:51 +0100

    LCL: CheckGroup/RadioGroup example

---
+++ lcl/CheckGroup/unit1.pas
@@ -0,0 +1,32 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    CheckGroup1: TCheckGroup;
+    RadioGroup1: TRadioGroup;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
