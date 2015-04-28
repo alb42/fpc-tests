bc2040b69ae5af7589b9276c260e86eaca9ac07d
alb <alb42@web.de>
2015-04-28 16:33:32 +0100

    lcl: ShellTreeView added

--- lcl/StringGrid/unit1.pas
+++ lcl/StringGrid/unit1.pas
@@ -6,18 +6,23 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Grids,
-  ExtCtrls, ComCtrls, ValEdit;
+  ExtCtrls, ComCtrls, ValEdit, ShellCtrls;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    ImageList1: TImageList;
     Panel1: TPanel;
     Panel2: TPanel;
+    Panel3: TPanel;
+    ShellTreeView1: TShellTreeView;
     StringGrid1: TStringGrid;
+    TreeView1: TTreeView;
     ValueListEditor1: TValueListEditor;
-    procedure Panel2Click(Sender: TObject);
+    procedure ShellTreeView1CustomDrawItem(Sender: TCustomTreeView;
+      Node: TTreeNode; State: TCustomDrawState; var DefaultDraw: Boolean);
     procedure StringGrid1MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
     procedure StringGrid1SelectCell(Sender: TObject; aCol, aRow: Integer;
@@ -37,16 +42,16 @@ implementation
 
 { TForm1 }
 
-
-procedure TForm1.Panel2Click(Sender: TObject);
-begin
-
-end;
-
 procedure TForm1.StringGrid1MouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
   writeln('mouse down ', X, ', ',Y, ', ', Button);
+end;
+
+procedure TForm1.ShellTreeView1CustomDrawItem(Sender: TCustomTreeView;
+  Node: TTreeNode; State: TCustomDrawState; var DefaultDraw: Boolean);
+begin
+  Node.ImageIndex:=Node.AbsoluteIndex;
 end;
 
 procedure TForm1.StringGrid1SelectCell(Sender: TObject; aCol, aRow: Integer;
