76a97f1d8403abfd32e15d92a0ee2a52c2b94092
alb <alb42@web.de>
2015-04-28 22:24:13 +0100

    LCL: Trackbar Test

---
+++ lcl/TrackBar/unit1.pas
@@ -0,0 +1,31 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    TrackBar1: TTrackBar;
+    TrackBar2: TTrackBar;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
